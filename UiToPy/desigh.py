2f2e25912612e5c1dcbc59ce59817e72a53bab11
Ivan Karasyov <vk-vk.karasev@yandex.ru>
2022-08-07 23:27:48 +0400

    + 2 apps
    Calculator App & UiToPy

---
+++ UiToPy/desigh.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'desigh.ui'
+##
+## Created by: Qt User Interface Compiler version 6.3.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QAction, QBrush, QColor, QConicalGradient,
+    QCursor, QFont, QFontDatabase, QGradient,
+    QIcon, QImage, QKeySequence, QLinearGradient,
+    QPainter, QPalette, QPixmap, QRadialGradient,
+    QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QMainWindow, QMenu,
+    QMenuBar, QPushButton, QSizePolicy, QVBoxLayout,
+    QWidget)
+
+import resurses
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(330, 128)
+        icon = QIcon()
+        icon.addFile(u":/icon/icon.png", QSize(), QIcon.Normal, QIcon.Off)
+        MainWindow.setWindowIcon(icon)
+        self.act_open = QAction(MainWindow)
+        self.act_open.setObjectName(u"act_open")
+        self.act_save = QAction(MainWindow)
+        self.act_save.setObjectName(u"act_save")
+        self.act_exit = QAction(MainWindow)
+        self.act_exit.setObjectName(u"act_exit")
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.verticalLayout_2 = QVBoxLayout(self.centralwidget)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
+        self.btn = QPushButton(self.centralwidget)
+        self.btn.setObjectName(u"btn")
+
+        self.verticalLayout_2.addWidget(self.btn)
+
+        self.label = QLabel(self.centralwidget)
+        self.label.setObjectName(u"label")
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.verticalLayout_2.addWidget(self.label)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 330, 26))
+        self.menu = QMenu(self.menubar)
+        self.menu.setObjectName(u"menu")
+        MainWindow.setMenuBar(self.menubar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.btn.setText(QCoreApplication.translate("MainWindow", u"\u041a\u043e\u043d\u0432\u0435\u0440\u0442\u0438\u0440\u043e\u0432\u0430\u0442\u044c", None))
+        self.label.setText(QCoreApplication.translate("MainWindow", u"\u041e\u0442\u043a\u0440\u043e\u0439 \u0444\u0430\u0439\u043b \u0432 \u043c\u0435\u043d\u044e \u0432\u044b\u0448\u0435 \u0438 \u043d\u0430\u0436\u043c\u0438 \u043a\u043d\u043e\u043f\u043a\u0443", None))
+        self.menu.setTitle(QCoreApplication.translate("MainWindow", u"\u0424\u0430\u0439\u043b", None))
+    # retranslateUi
+
---
+++ UiToPy/main.py
@@ -0,0 +1,78 @@
+import sys, os
+
+from PySide6.QtWidgets import QApplication, QMainWindow, QMenu, QMenuBar, QFileDialog, QMessageBox
+from desigh import Ui_MainWindow
+
+
+class Converter(QMainWindow):
+    def __init__(self):
+        super(Converter, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.path_file_in = None
+
+        # menubar
+        self.menuBar = QMenuBar(self)
+        self.setMenuBar(self.menuBar)
+
+        # menu
+        fileMenu = QMenu("&Файл", self)
+        self.menuBar.addMenu(fileMenu)
+        fileMenu.addAction("Открыть", self.open_file)
+        fileMenu.addSeparator()
+        fileMenu.addAction("Выход", sys.exit)
+
+        # buttons
+        self.ui.btn.clicked.connect(self.convert_file)
+
+    def convert_file(self) -> None:
+        """Функция выполняет конвертацию файлов. Тип файла определяет самостоятельно, на выход предлагает диагологое
+        окно для указания пути сохранения"""
+
+        if self.path_file_in is None:
+            error = QMessageBox()
+            error.setWindowTitle("Ошибка")
+            error.setText("Выберите файл в верхнем меню, а затем нажмите \"Конвертировать\"")
+            error.setIcon(QMessageBox.Warning)
+            error.setStandardButtons(QMessageBox.Ok)
+            error.exec()
+        else:
+            fname = QFileDialog.getSaveFileName(self)[0]
+            if str(self.path_file_in).endswith('.qrc'):
+                os.system(f'pyside6-rcc {self.path_file_in} -o {fname + ".py"}')
+                error = QMessageBox()
+                error.setWindowTitle("Инфорация")
+                error.setText("Успешно!")
+                error.setIcon(QMessageBox.Information)
+                error.setStandardButtons(QMessageBox.Ok)
+                error.exec()
+            elif str(self.path_file_in).endswith('.ui'):
+                os.system(f'pyside6-uic {self.path_file_in} -o {fname + ".py"}')
+                error = QMessageBox()
+                error.setWindowTitle("Инфорация")
+                error.setText("Успешно!")
+                error.setIcon(QMessageBox.Information)
+                error.setStandardButtons(QMessageBox.Ok)
+                error.exec()
+            else:
+                error = QMessageBox()
+                error.setWindowTitle("Ошибка")
+                error.setText("Выбран не корректный файл.\nУбедитесь в том, что файл имеет расширение .ui или .qrc")
+                error.setIcon(QMessageBox.Warning)
+                error.setStandardButtons(QMessageBox.Ok)
+                error.exec()
+
+    def open_file(self) -> None:
+        """Функция открытия файла в MenuBar"""
+
+        fname = QFileDialog.getOpenFileName(self)[0]
+        self.path_file_in = fname
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = Converter()
+    window.show()
+
+    sys.exit(app.exec())
